773f384d268d9acf63231e3ed651de4e14b25c24
Aleksandra Solomina <alessandrasolomina@yandex.ru>
2023-02-01 14:04:23 +0500

    parser

---
+++ JsonToXlsx parser/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonToXlsx_parser
+{
+    internal class Message
+    {
+            public string From { get; set; }
+            public string Date { get; set; }
+            public string Text { get; set; }
+     }
+    
+
+}
---
+++ JsonToXlsx parser/Program.cs
@@ -0,0 +1,67 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using Newtonsoft.Json;
+
+namespace JsonToXlsx_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // getting json string 
+            IEnumerable<string> lines = File.ReadLines("result.json");
+            string json = $@"{String.Join("", lines)}";
+
+            // using jarray from newtonsoft to get messages
+            var dynamicObject = JsonConvert.DeserializeObject<dynamic>(json)!;
+            var name = dynamicObject.name;
+            var messagesJarray = dynamicObject.messages;
+
+            //creating a list from JArray, for easier use of split for datetime
+            var records = new List<Message>();
+            foreach (var message in messagesJarray)
+            {
+                records.Add(new Message { From = message.from.ToString(), Date = message.date.ToString(), Text = message.text.ToString() });
+            }
+
+            // creating workbook
+            IWorkbook workbook = new XSSFWorkbook();
+            ISheet sheet1 = workbook.CreateSheet("Sheet1");
+
+            // setting headers
+            IRow row1 = sheet1.CreateRow(0);
+            row1.CreateCell(0).SetCellValue("From");
+            row1.CreateCell(1).SetCellValue("Date");
+            row1.CreateCell(2).SetCellValue("Time");
+            row1.CreateCell(3).SetCellValue("Text");
+
+            // wrting data from messages to workbook
+            for (int i = 0; i < messagesJarray.Count; i++)
+            {   
+                // checking if 
+                IRow row = sheet1.CreateRow(i + 1);
+
+                string[] dt = records[i].Date.Split(' ');
+                row.CreateCell(0).SetCellValue(records[i].From.ToString());
+                row.CreateCell(1).SetCellValue(dt[0].ToString());
+                row.CreateCell(2).SetCellValue(dt[1].ToString());
+                if (records[i].Text.ToString() != "")
+                {
+                   row.CreateCell(3).SetCellValue(records[i].Text.ToString());
+                }
+                else
+                {
+                    row.CreateCell(3).SetCellValue("message contained no text");
+                }
+
+            }
+
+                // saving workbook and closing
+            FileStream sw = File.Create($"{name}.xlsx");
+            workbook.Write(sw, false);
+            sw.Close();
+            Console.WriteLine("Json successfully parsed to xlsx");
+            }
+        }
+    }
+
